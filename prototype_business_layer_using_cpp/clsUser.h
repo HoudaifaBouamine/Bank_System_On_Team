ba6f8039822e174bf455a5cd15d3ef957f96466e
IsaacBh1 <isaac2548623@gmail.com>
2023-09-12 08:11:47 -0400

    remove the strucure that has the informations if user and redefine the functions of searching

--- prototype_business_layer_using_cpp/clsUser.h
+++ prototype_business_layer_using_cpp/clsUser.h
@@ -1,55 +1,63 @@
 #include "clsPerson.h" 
 
-
-
 class clsUser :public clsPerson
 {
-public :
-    struct stUser
-    {
-        string ID,FirstName , LastName , Email , Password ;
-        int Permition ; 
-        stFullAdress Address ;  
-    };
-    int Permition ; 
-    string Email,Password,Phone;
-
-    clsUser(int ID,string FirstName,string LastName,stFullAdress Adress , int Permetion ,string Email, string Password,string Phone):clsPerson(ID, FirstName, LastName, Adress){
+private:
+    int IDUser ; 
+    clsUser(string FirstName,string LastName,stFullAdress Adress , int Permetion ,string Email, string Password,string Phone , int IDUser):clsPerson(FirstName, LastName, Adress){
         this->Permition = Permition ; 
         this->Email = Email ; 
         this->Password = Password ; 
         this->Phone = Phone ; 
+        this->IDUser = IDUser ; 
     } 
 
-    bool UserPermition(int UserPermition , int IsHasPermition) {
-        return (UserPermition&IsHasPermition == IsHasPermition) ? true : false ; 
+public :
+    int Permition ; 
+    static  Email,Password,Phone;
+    static int ArrayLength = 0 ; 
+    statis clsUser ArrayOfUsers[]; 
+    
+
+    bool UserPermition(int IsHasPermition ){
+        return (Permition&IsHasPermition == IsHasPermition) ? true : false ; 
     }
 
-    bool Find_UserWithEmail(stUser ArrayOfUsers[1000] ,int ArrayLenght ,string Password , string Email){
-        for(int i = 0 ; i< ArrayLenght; i++  ) {
+    clsUser* Find_UserWithEmail(int ArrayLenght ,string Password , string Email){
+        for(int i = 0 ; i< ArrayLenght; i++){
             if (ArrayOfUsers[i].Password ==  Password && ArrayOfUsers[i].Email ==  Email) {
-                return true ; 
+                return & ArrayOfUsers[i]; 
             }
-            return false ; 
+            return NULL ;
+
         }
     } 
 
-    bool Find_UserWithID(stUser ArrayOfUsers[1000] ,int ArrayLenght ,string Password , string ID){
-        for(int i = 0 ; i< ArrayLenght; i++  ) {
+    clsUser* Find_UserWithID(int ArrayLenght ,string Password , string ID){
+        for(int i = 0 ; i< ArrayLenght; i++) {
             if (ArrayOfUsers[i].Password ==  Password && ArrayOfUsers[i].ID ==  ID) {
-                return true ; 
+                return & ArrayOfUsers[i] ; 
+                 
             }
-            return false ; 
+            return NULL ; 
+        }
+    } 
+    clsUser* Find_UserWithID(int ArrayLenght ,string Email){
+        for(int i = 0 ; i< ArrayLenght; i++) {
+            if (ArrayOfUsers[i].Email == Email ) {
+                return & ArrayOfUsers[i] ; 
+                 
+            }
+            return NULL ; 
         }
     } 
 
-    void Delete(stUser ArrayOfUsers[1000] ,int ArrayLenght ,string Password , string Email ,string ID){
+    void Delete(int ArrayLenght ,string Password , string Email ,string ID){
         for(int i = 0 ; i< ArrayLenght; i++  ) {
             if (ArrayOfUsers[i].Password ==  Password && ((ArrayOfUsers[i].ID ==  ID )|| (ArrayOfUsers[i].Email ==  Email) )) {
-                
                 ArrayOfUsers[i].Password = "";
                 ArrayOfUsers[i].ID =  "" ;  
-                ArrayOfUsers[i].FirstName ==  "" ; 
+                ArrayOfUsers[i].FirstName =  "" ; 
                 ArrayOfUsers[i].LastName = "";
                 ArrayOfUsers[i].Permition = 0 ;  
                 ArrayOfUsers[i].Email =  "" ; 
@@ -64,9 +72,8 @@ public :
         cout << "User Not Found" << endl ; 
     }
 
-    void Create_User(stUser ArrayOfUsers[1000] ,int ArrayLenght , stUser NewUser){
+    void Create_User(int ArrayLenght , clsUser NewUser){
         ArrayOfUsers[ArrayLenght] = NewUser;
-
     }
 
 } ; 
