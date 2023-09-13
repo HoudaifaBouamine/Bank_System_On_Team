50e17f4c13e7eeeb5840c72449559aeb61de71a3
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-09-13 15:36:08 +0100

    Creating Find User Function On DATA ACCESS Layer

--- Bank_Data_Layer/DataAccessLayer.cs
+++ Bank_Data_Layer/DataAccessLayer.cs
@@ -71,5 +71,66 @@ namespace Bank_Data_Layer
             return isFound;
         }
 
+        static public bool Find_User_By_ID
+            (
+            int User_ID, ref int Person_ID, ref string FirstName, ref string LastName,
+            ref string Country, ref string City, ref string Street,
+            ref string Email, ref string Password,
+            ref string Phone, ref int Permission
+            )
+
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "Select Persons.FirstName,Persons.LastName,Persons.Country,Persons.City,Persons.Street ,Users.* from Users Inner join Persons on Persons.Person_ID = Users.Person_ID WHERE Users.[User_ID] = @User_ID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@User_ID", User_ID);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    Person_ID = (int)reader["Person_ID"];
+                    FirstName = (string)reader["FirstName"];
+                    LastName = (string)reader["LastName"];
+                    Country = (string)reader["Country"];
+                    City = (string)reader["City"];
+                    Street = (string)reader["Street"];
+                    Email = (string)reader["Email"];
+                    Password = (string)reader["Password"];
+                    Phone = (string)reader["Phone"];
+                    Permission = (int)reader["Permission"];
+
+
+                    isFound = true;
+                }
+                else
+                {
+                    isFound = false;
+                }
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                isFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return isFound;
+        }
+
     }
 }
