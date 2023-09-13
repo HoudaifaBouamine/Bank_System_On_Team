767bbfb8d1c2905b2343e33b642ad9aa0fafda31
IsaacBh1 <isaac2548623@gmail.com>
2023-09-13 09:41:36 -0400

    creating the class ClsUser with constractor

--- Bank_Business_Layer/User.cs
+++ Bank_Business_Layer/User.cs
@@ -9,7 +9,21 @@ using Bank_Data_Layer;
 namespace Bank_Business_Layer
 {
     public class clsUser : clsPerson
-    {
+{
+    public string User_ID { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public int Permission { get; set; }
 
+    public User(string person_ID, string firstName, string lastName, string country, string city, string street, string user_ID, string email, string password, string phone, int permission)
+        : base(person_ID, firstName, lastName, country, city, street)
+    {
+        User_ID = user_ID;
+        Email = email;
+        Password = password;
+        Phone = phone;
+        Permission = permission;
     }
+}
 }
