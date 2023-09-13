148ef911999c55a33890506082c88cf8121ac937
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-09-13 14:09:30 +0100

    Creating clsPerson

--- Bank_Business_Layer/Person.cs
+++ Bank_Business_Layer/Person.cs
@@ -10,6 +10,22 @@ namespace Bank_Business_Layer
 {
     public class clsPerson
     {
+        public int Person_ID { get;}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
 
+        protected clsPerson(int person_ID, string firstName, string lastName, string country, string city, string street)
+        {
+            Person_ID = person_ID;
+            FirstName = firstName;
+            LastName = lastName;
+            Country = country;
+            City = city;
+            Street = street;
+        }
     }
+
 }
