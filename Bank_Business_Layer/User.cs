6b59dab614849ce96d4ba900bb8b56c4d6a76c36
HoudaifaBouamine <119418687+HoudaifaBouamine@users.noreply.github.com>
2023-09-13 14:45:26 +0100

    Correcting Errors on User.cs

--- Bank_Business_Layer/User.cs
+++ Bank_Business_Layer/User.cs
@@ -10,13 +10,13 @@ namespace Bank_Business_Layer
 {
     public class clsUser : clsPerson
 {
-    public string User_ID { get; set; }
+    public string User_ID { get; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string Phone { get; set; }
     public int Permission { get; set; }
 
-    public User(string person_ID, string firstName, string lastName, string country, string city, string street, string user_ID, string email, string password, string phone, int permission)
+    public clsUser(int person_ID, string firstName, string lastName, string country, string city, string street, string user_ID, string email, string password, string phone, int permission)
         : base(person_ID, firstName, lastName, country, city, street)
     {
         User_ID = user_ID;
