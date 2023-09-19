58c85f3b7381b202b0aa4e7db647dff1a9d6debf
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-09-20 00:56:22 +0100

    Creating clsTransaction Class

--- Bank_Business_Layer/Transaction.cs
+++ Bank_Business_Layer/Transaction.cs
@@ -8,8 +8,36 @@ using Bank_Data_Layer;
 
 namespace Bank_Business_Layer
 {
+
+    using System;
+
     public class clsTransaction
     {
 
+        public DataTable List()
+        {
+
+        }
+
+        public int Transaction_ID { get; private set; }
+        public clsClient Sender { get; set; }
+        public clsClient Receiver { get; set; }
+        public clsUser User { get; set; }
+        public int TransactionType_ID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+
+        private clsTransaction(int transactionID, clsClient sender, clsClient receiver, clsUser user, int transactionTypeID, decimal amount, DateTime transactionDateTime)
+        {
+            Transaction_ID = transactionID;
+            Sender = sender;
+            Receiver = receiver;
+            User = user;
+            TransactionType_ID = transactionTypeID;
+            Amount = amount;
+            TransactionDateTime = transactionDateTime;
+        }
     }
+
+
 }
