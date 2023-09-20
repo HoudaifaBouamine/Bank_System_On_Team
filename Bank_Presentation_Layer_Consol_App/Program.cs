5fb5f4ca43fb8e2c8caa3f5f0612206c136e3cfe
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-09-20 23:06:10 +0100

    Encapsulate all the test function on consol app presentation layer on one class

--- Bank_Presentation_Layer_Consol_App/Program.cs
+++ Bank_Presentation_Layer_Consol_App/Program.cs
@@ -14,23 +14,40 @@ namespace Bank_Presentation_Layer_Consol_App
     internal class Program
     {
 
+
+        static void Main(string[] args)
+        {
+
+              
+
+            Console.ReadKey();
+        }
+
+    }
+
+
+
+
+    class test
+    {
+
         static void print_user(clsUser user)
         {
 
             Console.WriteLine($"===============================");
             Console.WriteLine($"=      User Info              =");
             Console.WriteLine($"===============================");
-            Console.WriteLine($" User_ID    : {user.User_ID   }");
-            Console.WriteLine($" Person_ID  : {user.Person_ID }");
-            Console.WriteLine($" UserName  : {user.UserName }");
-            Console.WriteLine($" FirstName  : {user.FirstName }");
-            Console.WriteLine($" LastName   : {user.LastName  }");
-            Console.WriteLine($" Country    : {user.Country   }");
-            Console.WriteLine($" City       : {user.City      }");
-            Console.WriteLine($" Street     : {user.Street    }");
-            Console.WriteLine($" Email      : {user.Email     }");
-            Console.WriteLine($" Password   : {user.Password  }");
-            Console.WriteLine($" Phone      : {user.Phone     }");
+            Console.WriteLine($" User_ID    : {user.User_ID}");
+            Console.WriteLine($" Person_ID  : {user.Person_ID}");
+            Console.WriteLine($" UserName  : {user.UserName}");
+            Console.WriteLine($" FirstName  : {user.FirstName}");
+            Console.WriteLine($" LastName   : {user.LastName}");
+            Console.WriteLine($" Country    : {user.Country}");
+            Console.WriteLine($" City       : {user.City}");
+            Console.WriteLine($" Street     : {user.Street}");
+            Console.WriteLine($" Email      : {user.Email}");
+            Console.WriteLine($" Password   : {user.Password}");
+            Console.WriteLine($" Phone      : {user.Phone}");
             Console.WriteLine($" Permission : {user.Permission}");
             Console.WriteLine($"===============================\n\n");
         }
@@ -49,11 +66,11 @@ namespace Bank_Presentation_Layer_Consol_App
             }
         }
 
-        static void update_user_phone(int id,string phone)
+        static void update_user_phone(int id, string phone)
         {
-            clsUser user =clsUser.Find(id);
+            clsUser user = clsUser.Find(id);
 
-            if(user != null)
+            if (user != null)
             {
                 user.Phone = phone;
 
@@ -69,18 +86,18 @@ namespace Bank_Presentation_Layer_Consol_App
             clsUser new_user = new clsUser();
 
 
-            new_user.FirstName  = "Houdaifa";
-            new_user.LastName   = "Bouamine";
+            new_user.FirstName = "Houdaifa";
+            new_user.LastName = "Bouamine";
             new_user.UserName = "Admin";
-            new_user.Country    = "Algeria";
-            new_user.City       = "Souk-Ahras";
-            new_user.Street     = "Home";
-            new_user.Email      = "houdaifa@gmail.com";
-            new_user.Password   = "password123";
-            new_user.Phone      = "0987654321";
+            new_user.Country = "Algeria";
+            new_user.City = "Souk-Ahras";
+            new_user.Street = "Home";
+            new_user.Email = "houdaifa@gmail.com";
+            new_user.Password = "password123";
+            new_user.Phone = "0987654321";
             new_user.Permission = -1;
 
-            if(new_user.Save())
+            if (new_user.Save())
             {
                 Console.WriteLine($"User saved succssfuly with id = {new_user.User_ID}");
             }
@@ -94,12 +111,12 @@ namespace Bank_Presentation_Layer_Consol_App
         {
             DataTable list_users = clsUser.list();
 
-            if(list_users.Rows.Count < 1)
+            if (list_users.Rows.Count < 1)
             {
                 Console.WriteLine("Empty list");
             }
 
-            foreach(DataRow user in list_users.Rows) print_user(new clsUser( user));
+            foreach (DataRow user in list_users.Rows) print_user(new clsUser(user));
         }
 
 
@@ -115,12 +132,12 @@ namespace Bank_Presentation_Layer_Consol_App
             }
 
 
-     
+
             foreach (DataRow user in list_users.Rows)
             {
                 Console.WriteLine($"{user["User_ID"],-6} | {user["UserName"],-15} | {user["FirstName"],-15} | {user["LastName"],-15} | {user["Permission"],-5}");
             }
-            
+
         }
         static void delete_user(int id)
         {
@@ -141,7 +158,7 @@ namespace Bank_Presentation_Layer_Consol_App
                 char yes_or_no = 'n';
                 yes_or_no = Console.ReadLine()[0];
 
-                if(yes_or_no == 'y')
+                if (yes_or_no == 'y')
                 {
                     if (clsUser.Delete(user.User_ID))
                     {
@@ -185,7 +202,7 @@ namespace Bank_Presentation_Layer_Consol_App
         {
             clsClient client = clsClient.Find(Client_ID);
 
-            if(client == null)
+            if (client == null)
             {
                 Console.WriteLine("Client Not FOund\n\n");
                 return false;
@@ -204,11 +221,11 @@ namespace Bank_Presentation_Layer_Consol_App
                 Console.Write("Are you sure to delete this client ? [y/n] >> ");
                 char yes = Console.ReadLine()[0];
 
-                if(yes == 'y')
+                if (yes == 'y')
                 {
                     clsClient.Delete(id);
                 }
-               
+
             }
         }
 
@@ -271,7 +288,7 @@ namespace Bank_Presentation_Layer_Consol_App
 
         static void transaction_list()
         {
-            
+
 
             DataTable trans_list = clsTransaction.Table();
 
@@ -312,19 +329,6 @@ namespace Bank_Presentation_Layer_Consol_App
                 }
             }
         }
-
-        static void Main(string[] args)
-        {
-
-            PrintTransactionsTable();
-                //transaction_list();
-            //add_new_client();
-            //find_client(17);
-            //delete_client(15);
-
-            //update_client(14);
-
-            Console.ReadKey();
-        }
     }
+
 }
