da3842b69f83866c9f4db7edd72efc282f616a78
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-09-25 17:01:26 +0100

    Split transaction screen into 2 layers, mainScreen that get called by client main screen and the other sub-transaction-screen that will be called by mainScreen

---
+++ Bank_Presentation_Layer_Windows_App/ClientScreen/ClientHome/Transfer/frm_TransferFirstScreen.cs
@@ -0,0 +1,28 @@
+﻿using Bank_Business_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank_Presentation_Layer_Windows_App.ClientScreen.ClientHome.Transfer
+{
+    public partial class frm_TransferFirstScreen : Form
+    {
+        frm_TransferMainScreen main = null;
+        clsClient client;
+        public frm_TransferFirstScreen(frm_TransferMainScreen main,clsClient client)
+        {
+            this.main = main;
+            this.client = client;
+
+            InitializeComponent();
+        }
+
+
+    }
+}
