b7454fac588f12f6981d2099353793b85d254cad
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-09-29 10:46:59 +0100

    Creating Util Foulder and impliement page controler

---
+++ Bank_Presentation_Layer_Windows_App/Util/clsPagesControler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Bank_Presentation_Layer_Windows_App.Util
+{
+    internal class clsPagesControler
+    {
+        Dictionary<string, Form> Forms;
+        public clsPagesControler(Panel Container,Dictionary<string,Form> forms)
+        {
+            Forms = forms;
+        }
+
+        public void open_page(string FormName)
+        {
+            Forms[FormName].BringToFront();
+            Forms[FormName].Select();
+        }
+    }
+}
