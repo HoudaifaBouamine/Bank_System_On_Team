d9b3ea08686c51007057eb2359fdff4f61776f52
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-04 00:13:36 +0100

    Fix UI and link other screens

--- Bank_Presentation_Layer_Web_App/Pages/Start/AddPhone.cshtml.cs
+++ Bank_Presentation_Layer_Web_App/Pages/Start/AddPhone.cshtml.cs
@@ -3,15 +3,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Bank_Presentation_Layer_Web_App.Pages.Start
 {
-    public class CreateAccountModel : PageModel
+    public class AddPhoneModel : PageModel
     {
         public void OnGet()
         {
-        }
-
-        public void post_Sign_Up()
-        {
-            RedirectToPage("Login");
         }
     }
 }
