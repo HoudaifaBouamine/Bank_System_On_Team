4c54fbc79bf61e35f3d8e63d7166e5af73407493
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-03 14:19:13 +0100

    Creating Web app project

---
+++ Bank_Presentation_Layer_Web_App/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bank_Presentation_Layer_Web_App.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
