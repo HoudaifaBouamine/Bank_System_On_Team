f2f3383c06a59c2a3929bcf6de61a668ca05f7be
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-12 12:53:25 +0100

    Creating & Contumizing the User Window

---
+++ Bank_Presentation_Layer_User_Windows_App/Start/frm_Login.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Bank_Presentation_Layer_User_Windows_App.Start
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Login));
+            SuspendLayout();
+            // 
+            // frm_Login
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Red;
+            ClientSize = new Size(1262, 673);
+            Font = new Font("Arial", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "frm_Login";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
--- Bank_Presentation_Layer_User_Windows_App/frm_Main.cs
+++ Bank_Presentation_Layer_User_Windows_App/frm_Main.cs
@@ -1,3 +1,6 @@
+using Bank_Presentation_Layer_User_Windows_App.Properties;
+using System.Runtime.InteropServices;
+
 namespace Bank_Presentation_Layer_User_Windows_App
 {
     public partial class frm_Main : Form
@@ -5,6 +8,48 @@ namespace Bank_Presentation_Layer_User_Windows_App
         public frm_Main()
         {
             InitializeComponent();
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+            this.Text = string.Empty;
+            this.ControlBox = false;
+            this.DoubleBuffered = true;
         }
+
+        //Drag Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void pnl_HeaderBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btn_Minimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_Maximize_Click(object sender, EventArgs e)
+        {
+
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                btn_Maximize.Image = Resources.img_maximizeFormON;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                btn_Maximize.Image = Resources.img_maximizeFormOFF;
+            }
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
     }
 }
