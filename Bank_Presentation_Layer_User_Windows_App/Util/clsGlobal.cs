91657b14c96ce0e182f535809846c71415a98d26
houdaifa bouamine <houdaifa.bouamine@gmail.com>
2023-10-13 12:06:17 +0100

    Adding global class , contain the system current user

---
+++ Bank_Presentation_Layer_User_Windows_App/Util/clsGlobal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank_Business_Layer;
+
+namespace Bank_Presentation_Layer_User_Windows_App.Util
+{
+    public class clsGlobal
+    {
+
+        private clsUser _CurrentUser = null;
+        public clsUser CurrentUser 
+        {
+            get
+            {
+                return _CurrentUser;
+            }
+            set
+            {
+                _CurrentUser = value;
+            }
+        }
+    }
+}
